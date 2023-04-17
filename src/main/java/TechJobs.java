6c2ace91c893d1cc7cfb293b72a88443157ae36f
ZhengwuZhang <zhengwu225@gmail.com>
2023-04-17 18:44:23 -0500

    change every thing

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -121,14 +121,14 @@ public class TechJobs {
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
         if (someJobs.isEmpty()) {
-            System.out.println("No Results");
+            System.out.print("No Results");
         } else {
             for (HashMap<String, String> job : someJobs) {
-                System.out.println("*****");
+                System.out.println("\n*****");
                 for (String key : job.keySet()) {
                     System.out.println(key + ": " + job.get(key));
                 }
-                System.out.println("*****\n");
+                System.out.println("*****");
             }
         }
     }
